db0be47da54a4b9a2d09e41e7c5bcb5dac123726
kapil agrawal <agrawalkapil543@gmail.com>
2019-08-28 17:22:34 +0530

    Create Q_003.py

---
+++ Q_003.py
@@ -0,0 +1,24 @@
+''' Q--
+Python program to check the validity of a Password
+In this program, we will be taking a password as a combination of alphanumeric characters along with special characters, and check whether the password is valid or not with the help of few conditions.
+
+Primary conditions for password validation :
+
+Minimum 8 characters.
+The alphabets must be between [a-z]
+At least one alphabet should be of Upper Case [A-Z]
+At least 1 number or digit between [0-9].
+At least 1 character from [ _ or @ or $ ].
+Examples:
+
+Input : R@m@_f0rtu9e$
+Output : Valid Password
+
+Input : Rama_fortune$
+Output : Invalid Password
+Explanation: Number is missing
+
+Input : Rama#fortu9e 
+Output : Invalid Password
+
+'''
